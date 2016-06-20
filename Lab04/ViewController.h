ea6e1ec45e4ae66e3b66d94c36f0789bce9049c6
Ricardo Gonzalez <Ricardo@macbook-pro-de-ricardo.local>
2016-06-19 23:28:00 -0500

    Initial Commit

---
+++ Lab04/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab04
+//
+//  Created by Ricardo Gonzalez on 19/06/16.
+//  Copyright © 2016 RGO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
